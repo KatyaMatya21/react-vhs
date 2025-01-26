27f007d0d4c7a43e8681436bda76001a1a5375c8
katyamatya21 <katyamatya21@gmail.com>
2025-01-26 13:50:52 +0100

    add fixes

--- src/components/reviewForm/ReviewForm.jsx
+++ src/components/reviewForm/ReviewForm.jsx
@@ -1,27 +1,11 @@
-import { useReducer } from "react";
+import { useFormReview } from "./useFormReview.js";
 import { Counter } from "../counter/Counter";
 import { FormGroup } from "../formGroup/FormGroup.jsx";
 import './ReviewForm.css';
-import {
-  reducer,
-  INITIAL_VALUE,
-  SET_NAME_ACTION,
-  SET_TEXT_ACTION,
-  SET_CLEAR_ACTION,
-  SET_RATING_DECREMENT_ACTION,
-  SET_RATING_INCREMENT_ACTION
-} from "./reducer.js"
 
 export const ReviewForm = () => {
-  const [form, dispatch] = useReducer(reducer, INITIAL_VALUE);
-
+  const { form, setName, setText, setRatingDecrement, setRatingIncrement, setClear } = useFormReview();
   const { name, text, rating } = form;
-
-  const setName = (value) => dispatch({ type: SET_NAME_ACTION, payload: value });
-  const setText = (value) => dispatch({ type: SET_TEXT_ACTION, payload: value });
-  const setRatingDecrement = () => dispatch({ type: SET_RATING_DECREMENT_ACTION });
-  const setRatingIncrement = () => dispatch({ type: SET_RATING_INCREMENT_ACTION });
-  const setClear = () => dispatch({ type: SET_CLEAR_ACTION });
 
   return (
     <form className='form'>
